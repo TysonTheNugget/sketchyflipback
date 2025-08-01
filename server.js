b4ba4c2344243f441f4e1763eb755944147622a7
TysonTheNugget <pokebitmap@gmail.com>
2025-08-01 19:45:42 -0400

    last working was one before thishuhufefwefwe

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://sketchyflips.vercel.app', 'http://localhost:3000'],
+        origin: ['https://sketchyflips.vercel.app', 'https://dapps.mymilio.xyz', 'http://localhost:3000'],
         methods: ['GET', 'POST']
     }
 });
